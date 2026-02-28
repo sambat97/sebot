2119e0e05e6be34ccf2ee3e90ba501c1f7df92f0
Naufal Azmi <testing@sambat.xyz>
2026-02-28 07:55:45 +0700

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,6 +4,8 @@ router = Router()
 
 from commands.start import router as start_router
 from commands.co import router as co_router
+from commands.admin import router as admin_router
 
 router.include_router(start_router)
 router.include_router(co_router)
+router.include_router(admin_router)
